4ae4d429fbca0c5f876616ee0e50666eaf5a4647
Кадымов Варис Хафизович <29452821@s.mfui.ru>
2026-02-10 14:47:35 +0300

    fsdfsd

---
+++ classwork.js
@@ -0,0 +1,20 @@
+//задача 5
+let num1 = + prompt ("Введите число")
+let num2 = + prompt ("Введите число")
+let num3 = + prompt ("Введите число") 
+let result
+result = num1 + num2 + num3
+let sum = result / 3
+ if (result >= 90 && result <= 100){
+    alert ("5")
+}
+else if (result >= 75 && result <= 89){
+    alert ("4")
+}
+else if (result >= 60 && result <= 75){
+    alert ("3")
+}
+else{
+    alert("2")
+}
+
